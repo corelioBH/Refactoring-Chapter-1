f04cce8944a8b2db896eee09555b4f506545028c
Marco Mendes <marco.mendes@arkhi.com.br>
2017-12-28 21:36:35 -0200

    Alteracao para uso de StringBuilder

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Customer {
 
     private String name;
-    private List<Rental> rentals = new ArrayList<Rental>();
+    private List<Rental> rentals = new ArrayList<>();
 
     public Customer(String name) {
         this.name = name;
@@ -21,20 +21,24 @@ public class Customer {
     }
 
     public String statement() {
-        String result = "Rental record for " + getName() + "\n";
-        for (Rental rental : rentals)
-            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
-        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
-        return result;
+        StringBuilder result = new StringBuilder(); 
+        result.append("Rental record for").append(getName()).append("\n");
+        for (Rental rental : rentals) {
+        	    result.append("\t").append(rental.getMovie().getTitle())
+        	    		  .append("\t").append(rental.getCharge()).append("\n");
+        }
+        result.append("Amount owed is ").append(getTotalCharge()).append("\n");
+        result.append("You earned ").append(getTotalFrequentRenterPoints())
+        		  .append(" frequent renter points");
+        return result.toString();
     }
 
-    public String htmlStatement() {
+    public String htmlStatement() {  // Pode ser refatorado ainda?
         String result = "<h1>Rental record for <b>" + getName() + "</b></h1>\n";
         for (Rental rental : rentals)
-            result += "<p>" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "</p>\n";
-        result += "<p>Amount owed is <b>" + String.valueOf(getTotalCharge()) + "</b></p>\n";
-        result += "<p>You earned <b>" + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points</b></p>";
+            result += "<p>" + rental.getMovie().getTitle() + "\t" + rental.getCharge() + "</p>\n";
+        result += "<p>Amount owed is <b>" + getTotalCharge() + "</b></p>\n";
+        result += "<p>You earned <b>" + getTotalFrequentRenterPoints() + " frequent renter points</b></p>";
         return result;
 
     }
